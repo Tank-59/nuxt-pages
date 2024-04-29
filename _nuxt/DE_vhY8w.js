89a840d679098dfcfc2dd16bc94b631554dda737
Tank-59 <caoyanqi15@126.com>
2024-04-29 07:49:20 +0000

    Deploying to gh-pages from @ Tank-59/nuxt-pages@403772ecc2a8e8b5538811e877ab2d71a5e53f88 🚀

---
+++ _nuxt/DE_vhY8w.js
@@ -0,0 +1,1 @@
+import{i as o,r as u,h as f,j as d,k as v,l,m as i,n as h,q as m}from"./D1FoHsLT.js";function U(t,a={}){const e=a.head||o();if(e)return e.ssr?e.push(t,a):p(e,t,a)}function p(t,a,e={}){const s=u(!1),n=u({});f(()=>{n.value=s.value?{}:d(a)});const r=t.push(n.value,e);return v(n,c=>{r.patch(c)}),m()&&(l(()=>{r.dispose()}),i(()=>{s.value=!0}),h(()=>{s.value=!1})),r}export{U as u};
