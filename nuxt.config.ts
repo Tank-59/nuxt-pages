403772ecc2a8e8b5538811e877ab2d71a5e53f88
CaoYanqi <caoyanqi15@126.com>
2024-04-29 15:38:03 +0800

    feat: github action

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   app: {
     baseURL: '/nuxt-pages',
-    buildAssetsDir: 'nuxt_assets', //Github Pages 构建部署时候，会忽略隐藏文件比如.开头文件，_开头的文件
+    // buildAssetsDir: 'nuxt_assets', //Github Pages 构建部署时候，会忽略隐藏文件比如.开头文件，_开头的文件
     head: {
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
@@ -22,7 +22,7 @@ export default defineNuxtConfig({
   build: {
     transpile: ['pinia-plugin-persistedstate'],
   },
-  experimental: {
-    payloadExtraction: false, // github action部署忽略，关闭后将采用传统的服务器端渲染方式
-  },
+  // experimental: {
+  //   payloadExtraction: false, // github action部署忽略，关闭后将采用传统的服务器端渲染方式
+  // },
 })
